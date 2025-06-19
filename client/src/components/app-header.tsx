62b3325402398e66b1d9be8335a27f9ac7002ee3
Kalpak43 <kalpakgoshikwar123@gmail.com>
2025-06-19 17:06:43 +0000

    UPDATE: removed export options

--- client/src/components/app-header.tsx
+++ client/src/components/app-header.tsx
@@ -5,7 +5,7 @@ import { CirclePlus, PanelLeftIcon } from "lucide-react";
 import useMediaQuery from "@/hooks/use-media-query";
 import UserOptions from "./user-options";
 import ConversationTree from "./conversation-tree";
-import ExportOptions from "./export-options";
+// import ExportOptions from "./export-options";
 
 function AppHeader() {
   return (
@@ -29,7 +29,7 @@ function AppHeader() {
       <div className="max-md:hidden flex items-center justify-between gap-2 sticky md:absolute top-0 max-md:inset-x-0 right-0 py-1 max-md:px-2 md:mr-3 md:mt-1 z-50 max-md:bg-card max-md:border-b">
         <SidebarProvider className="min-h-0 max-h-full" defaultOpen={false}>
           <div className="flex gap-2">
-            <ExportOptions />
+            {/* <ExportOptions /> */}
             <ConversationTree />
           </div>
         </SidebarProvider>
